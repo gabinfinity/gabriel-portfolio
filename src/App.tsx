20bb36aa6a8cebb10385f2b803969dbaea3009c1
gabinfinity <59806981+gabinfinity@users.noreply.github.com>
2023-12-21 15:29:35 -0300

    fix missing react

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/themes/global"
 import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
+import React from "react"
 
 
 function App() {
